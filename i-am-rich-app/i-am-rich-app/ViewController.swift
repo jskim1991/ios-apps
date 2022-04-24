a2b86cf89e857bd98fdf209d52fa5776666d749b
jskim1991 <tensaijskim1991@gmail.com>
2022-04-24 19:08:34 +0900

    i am rich app

---
+++ i-am-rich-app/i-am-rich-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i-am-rich-app
+//
+//  Created by Jaesik Kim on 2022/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
