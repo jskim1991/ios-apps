9d0158fddda524b8266a38e1c71de7733f2b89bc
jskim1991 <tensaijskim1991@gmail.com>
2022-07-07 14:39:07 +0900

    networking, json, protocol, extension, etc...

---
+++ Clima-iOS13-master/Clima/Model/WeatherManager.swift
@@ -0,0 +1,59 @@
+import Foundation
+import CoreLocation
+
+protocol WeatherManagerDelegate{
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+
+struct WeatherManager {
+    
+    var delegate: WeatherManagerDelegate?
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=db4028e02ba9a08156397c90d9a281d6"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        invokeRequest(with: urlString)
+    }
+    
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
+        print(urlString)
+        invokeRequest(with: urlString)
+    }
+    
+    func invokeRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if (error != nil) {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let weather = self.parseJson(safeData) {
+                        self.delegate?.didUpdateWeather(self, weather: weather)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJson(_ data: Data) -> WeatherModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: data)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            return weather
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+}
---
+++ WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Jaesik Kim on 2022/07/06.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
